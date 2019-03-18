17b09f8b4262723d93ca46898b14a9a24ce67b4f
ScottDenton <scottyd_14@hotmail.com>
2019-03-18 16:53:18 -0700

    added alternate solution

--- index.js
+++ index.js
@@ -21,3 +21,30 @@ function BoardMember(name, homeState, training){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
+
+// below also works
+// function BoardMember(name, homeState, training) {
+// 	this.name = name;
+// 	this.homeState = homeState;
+// 	this.training = training;
+//
+// 	this.veto = function() {
+// 		return 'No, I must disagree';
+// 	};
+//
+// 	this.approve = function() {
+// 		return 'You can do that!';
+// 	};
+//
+// 	this.doCharity = function() {
+// 		return 'I like to help people.';
+// 	};
+//
+// 	this.releasePressStatement = function() {
+// 		return 'You will see great things from Scuber.';
+// 	};
+//
+// 	this.sayHi = function() {
+// 		return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+// 	};
+// }
